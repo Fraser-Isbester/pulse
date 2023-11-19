b3de7c26fa832ef9036468ddd99b44603f24073c
Fraser <fraserisbester@gmail.com>
2023-11-19 12:13:37 -0800

    working

--- pulse/__init__.py
+++ pulse/__init__.py
@@ -2,6 +2,11 @@ from langchain.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.llms import Ollama
+from langchain import hub
+from langchain.chains import RetrievalQA
 
 
 def main():
@@ -12,17 +17,15 @@ def main():
 
     db = FAISS.from_documents(data, OpenAIEmbeddings())
 
-    query = "What is an Autonomous Agent?"
-    docs = db.similarity_search(query)
-    print(docs)
-
-    exit()
+    # query = "What is an Autonomous Agent?"
+    # docs = db.similarity_search(query)
+    # print(docs)
 
     # Generates a RAG Chain
-    chain = load_chain("zephyr", index)
+    chain = load_chain("zephyr", db)
 
     # Example query (replace with your actual use case)
-    query = "What is the capital of France?"
+    query = "What is an Autonomous Agent?"
     response = chain(query)
     print(response)
 
@@ -48,19 +51,19 @@ def create_embeddings(data):
     index.add(db_vectors)
     return index
 
-# # Load a locally downloaded AI model with Langchain
-# def load_chain(model_name, vectorstore):
-#     llm = Ollama(
-#         model=model_name,
-#         verbose=True,
-#         callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
-#     )
-#     qa_chain = RetrievalQA.from_chain_type(
-#         llm,
-#         retriever=vectorstore.as_retriever(),
-#         chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
-#     )
-#     return qa_chain
+# Load a locally downloaded AI model with Langchain
+def load_chain(model_name, vectorstore):
+    llm = Ollama(
+        model=model_name,
+        verbose=True,
+        callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+    )
+    qa_chain = RetrievalQA.from_chain_type(
+        llm,
+        retriever=vectorstore.as_retriever(),
+        chain_type_kwargs={"prompt": hub.pull("rlm/rag-prompt-llama")},
+    )
+    return qa_chain
 
 
 if __name__ == '__main__':

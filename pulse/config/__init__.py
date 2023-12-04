5b462aed2aff96c4e77a76186c26f99df2ba2490
Fraser Isbester <fraserisbester@gmail.com>
2023-12-03 17:47:39 -0800

    feat(vectorstore): add pgvector support (#11)
    Adds PGVector support.
    
    closes #3

--- pulse/config/__init__.py
+++ pulse/config/__init__.py
@@ -7,5 +7,7 @@ load_dotenv()
 
 vectorstore = environ.get("VECTORSTORE", "redis")
 redis_url = environ.get("REDIS_URL", "redis://redis:6379")
+postgres_url = environ.get("POSTGRES_URL", "postgresql+pg8000://postgres@postgres:5432")
+
 slack_token = environ["SLACK_BOT_TOKEN"]
 openai_api_key = environ["OPENAI_API_KEY"]

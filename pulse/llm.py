f199e3eb05d4000ee56a473e424bb39f5b4c7340
Fraser <fraserisbester@gmail.com>
2023-12-03 23:32:14 -0800

    refactor: move to vectorstore factory

--- pulse/llm.py
+++ pulse/llm.py
@@ -3,11 +3,10 @@ from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
+from langchain.schema.vectorstore import VectorStore
 
-from pulse.vectorstore import vectorstore
 
-
-def get_retriever(model_name: str = "gpt-4-1106-preview", vectorstore=vectorstore) -> RetrievalQA:
+def get_retriever(vectorstore: VectorStore, model_name: str = "gpt-4-1106-preview") -> RetrievalQA:
     """Builds the RAG Chain."""
 
     llm = ChatOpenAI(
